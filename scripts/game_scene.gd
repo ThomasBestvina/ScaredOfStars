6f80259a2bbaefbf202a30ce3e63ce3152c7b5d3
Thomas Bestvina <bestvinathomas@gmail.com>
2025-09-12 20:04:39 -0600

    e

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,1 +1,21 @@
 extends Node2D
+
+var floor_layer = []
+var object_layer = []
+var ceiling_layer = []
+
+@export var camera: Node2D
+
+func _ready() -> void:
+	# Initialize all layers with default values
+	for x in range(Config.VIEWPORT_TILE_SIZE):
+		var row = []
+		for y in range(Config.VIEWPORT_TILE_SIZE):
+			row.append([0, Color.WHITE])  # Default values
+		
+		floor_layer.append(row.duplicate())
+		object_layer.append(row.duplicate())
+		ceiling_layer.append(row.duplicate())
+
+func update() -> void:
+	pass
---
+++ scripts/singletons/config.gd
@@ -0,0 +1,4 @@
+extends Node
+
+const VIEWPORT_TILE_SIZE: int = 60
+const CHUNK_SIZE: int = 256
